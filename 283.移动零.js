e05c8512e8f0e1b9463e0f2e156dbf84558efcff
RayJason <i@rayjason.cn>
2021-11-11 15:57:27 +0800

    leetCode算法初级: D3 双指针

---
+++ 283.移动零.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  if (!nums || nums.length === 0) return
+
+  let slow = 0,
+    fast = 0
+  for (; fast < nums.length; fast++) {
+    if (nums[fast] != 0) {
+      // 1. nums[fast]指向不为0时，双指针都++
+      if (fast > slow) {
+        // 3. 此时fast领先slow，slow一定指向0
+        // 因此可以直接对调
+        nums[slow] = nums[fast]
+        nums[fast] = 0
+      }
+      // 4. 对调后slow++双指针又都指向0了
+      slow++
+    }
+    // 2. 当nums[fast]指向0时，只有fast++
+  }
+}
+// @lc code=end
